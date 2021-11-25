3b9c85792ca1a195f205aab47940a82ea7e8c2f7
Jesper Lundqvist <jesper.lundqvist@eyevinn.se>
2021-11-25 17:01:43 +0100

    Updated tests

--- test/vmaf.test.ts
+++ test/vmaf.test.ts
@@ -3,6 +3,6 @@ import getVmaf from '../src/get-vmaf';
 describe('vmaf', () => {
   it('Gets VMAF score from local file', async () => {
     const vmaf = await getVmaf('test/resources/vmaf.json');
-    expect(vmaf).toEqual(85.628489);
+    expect(vmaf[0].vmaf).toEqual(85.628489);
   });
 });

ea56249b8e1f8f4adb3beded3ac288fa723e8696
Jesper Lundqvist <jesper.lundqvist@eyevinn.se>
2021-11-29 12:27:05 +0100

    Rewrote suggestLadder to use internal getVmaf-function.
    This adds support for generating ABR-ladders from local files.

--- src/suggest-ladder.ts
+++ src/suggest-ladder.ts
@@ -1,9 +1,7 @@
-import { GetObjectCommand, ListObjectsV2Command, S3 } from '@aws-sdk/client-s3';
 import { Resolution } from './models/resolution';
-import { isS3URI } from './pipelines/aws/aws-pipeline';
-import path from 'path';
 import logger from './logger';
 import { BitrateResolutionVMAF } from './models/bitrate-resolution-vmaf';
+import { getVmaf } from '.';
 
 /**
  * Suggests an optimal ABR-ladder from a directory of VMAF-files. Only supports loading from S3 at the moment.
@@ -16,65 +14,34 @@ import { BitrateResolutionVMAF } from './models/bitrate-resolution-vmaf';
 export default async function suggestLadder(
   directoryWithVmafFiles: string,
   filterFunction: (bitrate: number, resolution: Resolution, vmaf: number) => boolean = () => true,
-  includeAllBitrates: boolean = false
+  includeAllBitrates: boolean = false,
+  onProgress: (index: number, filename: string, total: number) => void = () => {}
 ): Promise<BitrateResolutionVMAF[]> {
   let pairs = new Map<number, { resolution: Resolution; vmaf: number }[]>();
   let optimal: { resolution: Resolution; vmaf: number; bitrate: number }[] = [];
+
   logger.info('Loading VMAF data...');
-  if (isS3URI(directoryWithVmafFiles)) {
-    const uri = new URL(directoryWithVmafFiles);
-    const bucket = uri.hostname;
-    const key = uri.pathname.substring(1);
-    let s3 = new S3({});
-    const listCommand = new ListObjectsV2Command({ Bucket: bucket, Prefix: key });
-    const listResponse = await s3.send(listCommand);
+  const vmafs = await getVmaf(directoryWithVmafFiles, onProgress);
+  let counter = 1;
+  vmafs.forEach(({ filename, vmaf }) => {
+    const [resolutionStr, bitrateStr] = filename.split('_');
+    const [widthStr, heightStr] = resolutionStr.split('x');
 
-    const streamToString = (stream: any): Promise<string> =>
-      new Promise((resolve, reject) => {
-        const chunks: any[] = [];
-        stream.on('data', (chunk: any) => chunks.push(chunk));
-        stream.on('error', reject);
-        stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
-      });
+    const width = parseInt(widthStr);
+    const height = parseInt(heightStr);
+    const bitrate = parseInt(bitrateStr);
 
-    if (listResponse.Contents !== undefined) {
-      const getCommands = listResponse.Contents?.map(obj => new GetObjectCommand({ Bucket: bucket, Key: obj.Key }));
-      const getReponses = getCommands
-        .filter(command => command.input.Key?.endsWith('.json'))
-        .map(async command => {
-          const response = await s3.send(command);
-          return { key: command.input.Key!, response };
-        });
-      let counter = 1;
-      for (const promise of getReponses) {
-        const { key, response } = await promise;
-        const stream = (await response).Body;
-        const data = await streamToString(stream);
-        const vmafData = JSON.parse(data);
-        const filename = path.basename(key);
-        const [resolutionStr, bitrateStr] = filename.split('_');
-        const [widthStr, heightStr] = resolutionStr.split('x');
-
-        const vmaf = vmafData['pooled_metrics']['vmaf']['harmonic_mean'];
-        const width = parseInt(widthStr);
-        const height = parseInt(heightStr);
-        const bitrate = parseInt(bitrateStr);
-
-        if (filterFunction(bitrate, { width, height }, vmaf)) {
-          if (pairs.has(bitrate)) {
-            pairs.get(bitrate)?.push({ resolution: { width, height }, vmaf });
-          } else {
-            pairs.set(bitrate, [{ resolution: { width, height }, vmaf }]);
-          }
-        }
-
-        logger.info(`Finished loading VMAF ${counter}/${getReponses.length}.`);
-        counter += 1;
+    if (filterFunction(bitrate, { width, height }, vmaf)) {
+      if (pairs.has(bitrate)) {
+        pairs.get(bitrate)?.push({ resolution: { width, height }, vmaf });
+      } else {
+        pairs.set(bitrate, [{ resolution: { width, height }, vmaf }]);
       }
     }
-  } else {
-    // TODO: Load local VMAF-scores
-  }
+
+    logger.info(`Finished loading VMAF ${counter}/${vmafs.length}.`);
+    counter += 1;
+  });
 
   // Get optimal resolution for each bitrate
   pairs.forEach((bitrateVmafPairs, bitrate) => {

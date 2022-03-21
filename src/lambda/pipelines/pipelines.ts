d1125e948cca511c2f54af0d7fcfbdb09a3e4886
Oscar Nord <oscar.nord@gmail.com>
2022-03-21 21:21:55 +0100

    Update pipelines.ts

--- src/lambda/pipelines/pipelines.ts
+++ src/lambda/pipelines/pipelines.ts
@@ -1,10 +1,10 @@
 const default_pipeline = {
   "aws": {
     "s3Bucket": "vmaf-files",
-    "mediaConvertRole": "arn:aws:iam::590877988961:role/MediaConvert_Default_Role",
-    "mediaConvertEndpoint": "https://xnlmxec5a.mediaconvert.eu-north-1.amazonaws.com",
+    "mediaConvertRole": "your-media-convert-role",
+    "mediaConvertEndpoint": "your-media-convert-endpoint",
     "ecsSubnet": "subnet-05d98882c13408e16",
-    "ecsSecurityGroup": "sg-0e444b67a747bf739",
+    "ecsSecurityGroup": "your-ecs-security-group",
     "ecsContainerName": "easyvmaf-s3",
     "ecsCluster": "vmaf-runner",
     "ecsTaskDefinition": "easyvmaf-s3:3"

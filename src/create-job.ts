bce73d12ef4fb1ec3a3388fe7639fe28a80f2d6f
Oscar Nord <oscar.nord@gmail.com>
2022-05-02 11:21:53 +0200

    rename autoabr -> autovmaf (#19)
    * chore: rename autoabr -> autovmaf
    
    * Update README.md

--- src/create-job.ts
+++ src/create-job.ts
@@ -48,7 +48,7 @@ export type JobDescription = {
  *
  * @example **Example of creating a job from a job description.**
  * ```javascript
- * const { createJob, qualityAnalysisModelToString } = require('@eyevinn/autoabr');
+ * const { createJob, qualityAnalysisModelToString } = require('@eyevinn/autovmaf');
  *
  * const abrLadder = await = createJob({
  *   name: "hello",
@@ -64,7 +64,7 @@ export type JobDescription = {
  * ```
  * @example **Example of creating a job from a YAML-file.**
  * ```javascript
- * const { createJob, JobDescription } = require('@eyevinn/autoabr');
+ * const { createJob, JobDescription } = require('@eyevinn/autovmaf');
  * const YAML = require('yaml');
  * const fs = require('fs');
  *

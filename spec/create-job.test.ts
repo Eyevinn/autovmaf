86ed8ac704c580448d72cc29115dddb70f65d3fe
Oscar Nord <oscar.nord@gmail.com>
2022-05-03 15:46:48 +0200

    chore: fix workflow

--- spec/create-job.test.ts
+++ spec/create-job.test.ts
@@ -2,6 +2,7 @@ import { mockClient } from 'aws-sdk-client-mock';
 import { CreateJobCommand, GetJobCommand, MediaConvertClient } from '@aws-sdk/client-mediaconvert';
 import { HeadObjectCommand, S3Client, waitUntilObjectExists } from '@aws-sdk/client-s3';
 import { ECSClient } from '@aws-sdk/client-ecs';
+//import { createJob } from '../src/create-job';
 
 const mcMock = mockClient(MediaConvertClient);
 const ecsMock = mockClient(ECSClient);
@@ -11,4 +12,12 @@ beforeEach(() => {
   mcMock.reset();
   ecsMock.reset();
   s3Mock.reset();
+});
+
+
+// test the create-job method
+describe('create-job', () => {
+  it('should create a job successfully', async () => {
+    console.log('should create a job successfully');
+  });
 });

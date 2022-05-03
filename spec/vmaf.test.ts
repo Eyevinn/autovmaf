72c1ded94b635e6343cbab8e18cb3a7d67359a37
Oscar Nord <oscar.nord@gmail.com>
2022-05-03 18:19:26 +0200

    Add tests (#28)
    * chore: add jest and test workflow
    
    * Update test.yml
    
    * chore: fix workflow
    
    * chore: add simple test for createJob
    
    * chore: fix test
    
    * chore: minor cleanup
    
    * Create h265-encoding-profile.json

--- spec/vmaf.test.ts
+++ spec/vmaf.test.ts
@@ -2,7 +2,7 @@ import getVmaf from '../src/get-vmaf';
 
 describe('vmaf', () => {
   it('Gets VMAF score from local file', async () => {
-    const vmaf = await getVmaf('test/resources/vmaf.json');
+    const vmaf = await getVmaf('spec/resources/vmaf.json');
     expect(vmaf[0].vmaf).toEqual(85.628489);
   });
 });

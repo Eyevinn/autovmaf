cf72dfba3279a7a85806064abd5328d539be471d
Oscar Nord <oscar.nord@gmail.com>
2022-05-03 13:50:20 +0200

    Make suggest ladder optional (#22)
    * chore: make it possible to skip `suggestLadder`
    
    * chore: do not run suggestLadder when finished analyzing

--- src/create-job.ts
+++ src/create-job.ts
@@ -86,9 +86,8 @@ export type JobDescription = {
  * ```
  *
  * @param description An object that describes the job to create.
- * @returns The optimal ladder for each model.
  */
-export default async function createJob(description: JobDescription, pipelineData?: any, encodingProfileData?: any): Promise<{ model: QualityAnalysisModel; optimalLadder: BitrateResolutionVMAF[] }[]> {
+export default async function createJob(description: JobDescription, pipelineData?: any, encodingProfileData?: any) {
   logger.info(`Creating job ${description.name}.`);
 
   let pipeline: any = undefined;
@@ -118,15 +117,10 @@ export default async function createJob(description: JobDescription, pipelineDat
     'reference.mp4'
   );
 
-  let modelLadders: {
-    model: QualityAnalysisModel;
-    optimalLadder: BitrateResolutionVMAF[];
-  }[] = [];
-
   if (description.method === 'walkTheHull') {
-    modelLadders = await analyzeWalkTheHull();
+    await analyzeWalkTheHull();
   } else {
-    const data = await analyzeBruteForce(description.name, reference, pipeline, {
+    await analyzeBruteForce(description.name, reference, pipeline, {
       bitrates: description.bitrates,
       resolutions: description.resolutions,
       concurrency: true,
@@ -134,29 +128,7 @@ export default async function createJob(description: JobDescription, pipelineDat
       filterFunction:
         description.bitrates !== undefined && description.resolutions !== undefined ? _ => true : undefined,
     });
-    modelLadders = data;
-  }
-
-  if (description.output !== undefined && process.env.SKIP_FILEWRITE === 'undefined') {
-    for (const modelLadder of modelLadders) {
-      const modelStr = qualityAnalysisModelToString(modelLadder.model);
-      const outputFilename = description.output + '_' + modelStr + '.csv';
-      logger.info(`Writing results to ${outputFilename}...`);
-
-      const ladder = modelLadder.optimalLadder;
-
-      const csv = Papa.unparse(
-        ladder.map(rung => ({
-          bitrate: rung.bitrate,
-          resolution: `${rung.resolution.width}x${rung.resolution.height}`,
-          vmaf: rung.vmaf,
-        }))
-      );
-      fs.writeFileSync(outputFilename, csv);
-    }
   }
 
   logger.info('Finished analysis!');
-
-  return modelLadders;
 }
